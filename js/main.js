b1871ab43dde203cc3945c16caf09a1c6a6e9359
Anton Eliseev <anton_saintp@mail.ru>
2023-10-17 20:15:21 +0300

    added pin for yandex map

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ phoneInputs.forEach((input) => {
 ymaps.ready(init);
 function init(){
     // Создание карты.
-    var myMap = new ymaps.Map("map", {
+    var map = new ymaps.Map("map", {
         // Координаты центра карты.
         // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
@@ -40,4 +40,27 @@ function init(){
         // от 0 (весь мир) до 19.
         zoom: 16,
     });
+
+    var myPlacemark = new ymaps.Placemark(
+		[59.943543, 30.338928],
+		{
+			balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>
+			`,
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40],
+		},
+    );
+
+    map.geoObjects.add(myPlacemark);
+
 }
