0f2c5dfb884847ecba5ae3522272dedfe960f6a8
Anton Eliseev <anton_saintp@mail.ru>
2023-10-17 20:03:49 +0300

    added yandex map

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ navBtn.onclick = () => {
    document.body.classList.toggle('no_scroll')
 }
 
-/*Phone mask*/
+//Phone mask
+
 mask('[data-tel-input]');
 
 // Removing '+' if nothing else is entered to show placeholder
@@ -23,3 +24,20 @@ phoneInputs.forEach((input) => {
       if (input.value == '+') input.value = '';
    })
 });
+
+// Yandex map
+
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [59.943543,30.338928],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 16,
+    });
+}
