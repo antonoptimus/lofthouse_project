331beb8e90dd9ca7727a7ef75f753067fb4e5040
Anton Eliseev <anton_saintp@mail.ru>
2023-10-17 21:00:12 +0300

    ready yandex map

--- js/main.js
+++ js/main.js
@@ -61,6 +61,16 @@ function init(){
 		},
     );
 
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+ 
+   //  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+   //  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+ 
     map.geoObjects.add(myPlacemark);
-
+    myPlacemark.balloon.open();
 }
